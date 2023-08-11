6b4d5987c88f2fbe3fd0555c73d48ca0320e4e56
rikhsi <rikhsimboyev1997@gmail.com>
2023-08-11 05:19:56 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'speakers';
+}
